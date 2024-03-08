d09ce78a4431e6159df1a87859467eea352e87c8
ahmedjimmy3 <jimmyahmed881@gmail.com>
2024-03-08 06:21:05 -0800

    clean code

--- src/modules/Auth/auth.controller.js
+++ src/modules/Auth/auth.controller.js
@@ -1,8 +1,6 @@
 import User from '../../../db/models/user.model.js'
 import * as authServices from './auth.services.js'
-import {OAuth2Client} from 'google-auth-library'
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
+
 
 export const signUp = async(req,res,next)=>{
     const {username,email,password,phoneNumbers,addresses,age} = req.body
@@ -28,60 +26,13 @@ export const logIn = async(req,res,next)=>{
 
 export const loginWithGmail = async(req,res,next)=>{
     const {idToken} = req.body
-    const client = new OAuth2Client();
-    async function verify() {
-    const ticket = await client.verifyIdToken({
-        idToken,
-        audience: process.env.CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-        // Or, if multiple clients access the backend:
-        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-    });
-    const payload = ticket.getPayload();
-    return payload
-    }
-    const result = await verify().catch(console.error);
-    if(!result.email_verified){
-        throw({message:'Please verify your email first',cause:400})
-    }
-    const isUserExist = await User.findOne({email:result.email,provider:'GOOGLE'})
-    if(!isUserExist){
-        throw({message:'Invalid credentials',cause:400})
-    }
-    const token = jwt.sign({id:isUserExist._id,email:result.email,loggedIn:true} , process.env.JWT_SECRET_KEY)
-    isUserExist.isLoggedIn = true
-    await isUserExist.save()
+    const token = await authServices.loginWithGmailFunction(idToken)
     res.status(200).json({message:'You logged in successfully',data:token})
 }
 
 export const signUpWithGmail = async(req,res,next)=>{
     const {idToken} = req.body
-    const client = new OAuth2Client();
-    async function verify() {
-    const ticket = await client.verifyIdToken({
-        idToken,
-        audience: process.env.CLIENT_ID,
-    });
-    const payload = ticket.getPayload();
-    return payload
-    }
-    const result = await verify().catch(console.error);
-    if(!result.email_verified){
-        throw({message:'Please verify your email first',cause:400})
-    }
-
-    const isEmailExist  = await User.findOne({email:result.email})
-    if(isEmailExist){
-        throw({message:'This email already used',cause:409})
-    }
-    const randomPassword = Math.random().toString(36).slice(-8)
-    const hashedPassword = bcrypt.hashSync(randomPassword,+process.env.SALT_ROUNDS)
-    const newUser = await User.create({
-        username:result.name,
-        email:result.email,
-        password:hashedPassword,
-        isEmailVerified:true,
-        provider:'GOOGLE',
-    })
+    const newUser = await authServices.signUpWithGmailFunction(idToken)
     res.status(201).json({
         message:'User created successfully',
         data:newUser
--- src/modules/Auth/auth.services.js
+++ src/modules/Auth/auth.services.js
@@ -3,6 +3,7 @@ import * as authRepo from './auth.repo.js'
 import sendEmailService from '../services/send-email.services.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import {OAuth2Client} from 'google-auth-library'
 
 export const createUserFunction = async(user,req)=>{
     const isEmailExist  = await authRepo.findUserByEmail(user.email)
@@ -59,4 +60,59 @@ export const logInFunction = async(email,password)=>{
     isUserExist.isLoggedIn = true
     await isUserExist.save()
     return token
+}
+
+export const loginWithGmailFunction=  async(idToken)=>{
+    const client = new OAuth2Client();
+    async function verify() {
+    const ticket = await client.verifyIdToken({
+        idToken,
+        audience: process.env.CLIENT_ID,  
+    });
+    const payload = ticket.getPayload();
+    return payload
+    }
+    const result = await verify().catch(console.error);
+    if(!result.email_verified){
+        throw({message:'Please verify your email first',cause:400})
+    }
+    const isUserExist = await User.findOne({email:result.email,provider:'GOOGLE'})
+    if(!isUserExist){
+        throw({message:'Invalid credentials',cause:400})
+    }
+    const token = jwt.sign({id:isUserExist._id,email:result.email,loggedIn:true} , process.env.JWT_SECRET_KEY)
+    isUserExist.isLoggedIn = true
+    await isUserExist.save()
+    return token
+}
+
+export const signUpWithGmailFunction = async(idToken)=>{
+    const client = new OAuth2Client();
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken,
+            audience: process.env.CLIENT_ID,
+        });
+        const payload = ticket.getPayload();
+        return payload
+        }
+        const result = await verify().catch(console.error);
+        if(!result.email_verified){
+            throw({message:'Please verify your email first',cause:400})
+        }
+    
+        const isEmailExist  = await User.findOne({email:result.email})
+        if(isEmailExist){
+            throw({message:'This email already used',cause:409})
+        }
+        const randomPassword = Math.random().toString(36).slice(-8)
+        const hashedPassword = bcrypt.hashSync(randomPassword,+process.env.SALT_ROUNDS)
+        const newUser = await User.create({
+            username:result.name,
+            email:result.email,
+            password:hashedPassword,
+            isEmailVerified:true,
+            provider:'GOOGLE',
+        })
+        return newUser
 }
